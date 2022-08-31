904156e07975b84c4be40a90f5437806aa03f75f
Sebastian Schmidt <isibboi@gmail.com>
2022-08-31 10:05:02 +0300

    Add counter example.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
-fn main() {
-    println!("Hello, world!");
+use iced::widget::{button, Button, Column, Text};
+use iced::{Alignment, Element, Sandbox, Settings};
+
+pub fn main() -> iced::Result {
+    Counter::run(Settings::default())
+}
+
+struct Counter {
+    inc_button_state: button::State,
+    dec_button_state: button::State,
+    value: i32,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    IncrementPressed,
+    DecrementPressed,
+}
+
+impl Sandbox for Counter {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self {
+            inc_button_state: Default::default(),
+            dec_button_state: Default::default(),
+            value: 0,
+        }
+    }
+
+    fn title(&self) -> String {
+        String::from("Counter - Iced")
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::IncrementPressed => {
+                self.value += 1;
+            }
+            Message::DecrementPressed => {
+                self.value -= 1;
+            }
+        }
+    }
+
+    fn view(&mut self) -> Element<Message> {
+        Column::new()
+            .push(
+                Button::new(&mut self.inc_button_state, Text::new("Increment"))
+                    .on_press(Message::IncrementPressed),
+            )
+            .push(Text::new(self.value.to_string()).size(50))
+            .push(
+                Button::new(&mut self.dec_button_state, Text::new("Decrement"))
+                    .on_press(Message::DecrementPressed),
+            )
+            .padding(20)
+            .align_items(Alignment::Center)
+            .into()
+    }
 }
